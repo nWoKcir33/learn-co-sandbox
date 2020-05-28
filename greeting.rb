cd0bd6069d7d4009766cd6fb59dbb99621a24aae
Rick Lim <rick.jason.a.lim@gmail.com>
2020-05-28 03:01:35 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,6 @@
+english_bands_by_city = {
+  :liverpool => "The beatles",
+  :manchester => "The Smiths",
+  :coventry => "Delia Derbyshire and the BBC Radiophonic Band",
+  :london => "Zigy Stardust and the Spiders from Mars"
+}
---
+++ if.rb
@@ -0,0 +1,9 @@
+name = gets.chomp
+
+if name == "Danny"
+  puts "Welcome Danny!"
+elsif name == "Ian"
+  puts "Hey Ian!"
+else
+  puts "Hello Stranger!"
+end
